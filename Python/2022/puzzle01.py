dabe13819a262be4ff80f0e0c932d808919d1390
Skulhunter5 <62426864+Skulhunter5@users.noreply.github.com>
2022-12-01 16:16:12 +0100

    Python solution for 2022 day 01

---
+++ Python/2022/puzzle01.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append('..')
+
+from common.Puzzle import Puzzle
+
+class Puzzle01(Puzzle, year=2022, day=1):
+    @staticmethod
+    def process_input(data):
+        return data
+
+    def elves(self):
+        i = 0
+        elves = []
+        amount = 0
+        while(i < len(self.data)):
+            if(self.data[i] == ''):
+                elves.append(amount)
+                amount = 0
+            else:
+                amount += int(self.data[i])
+            i += 1
+        if(amount != 0):
+            elves.append(amount)
+        return sorted(elves)
+
+    def solve_part_1(self): # Solution for part 1
+        return self.elves()[-1]
+    
+    def solve_part_2(self): # Solution for part 2
+        return sum(self.elves()[-3:])
+
+if(__name__ == "__main__"):
+    puzzle = Puzzle01()
+    part1, part2 = puzzle.solve()
+    print(f"Advent of Code {puzzle.year} day {puzzle.day}:")
+    print(f"- Part 1: {part1}")
+    print(f"- Part 2: {part2}")
