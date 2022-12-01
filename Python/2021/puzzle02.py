a047150308ddfc8f30f998debc36e3917fe6399d
Skulhunter5 <62426864+Skulhunter5@users.noreply.github.com>
2022-12-01 23:01:54 +0100

    Python solution 2021 day 02 alternatives

--- Python/2021/puzzle02.py
+++ Python/2021/puzzle02.py
@@ -19,6 +19,11 @@ class Puzzle02(Puzzle, year=2021, day=2):
                 position += x
         return position * depth
 
+    def part_1_alternative(self):
+        position = sum([x for (command, x) in self.data if command == "forward"])
+        depth = sum([(x if command == "down" else -x) for (command, x) in self.data if command != "forward"])
+        return position * depth
+
     def solve_part_2(self): # Solution for part 2
         aim = position = depth = 0
         for (command, x) in self.data:
@@ -29,6 +34,11 @@ class Puzzle02(Puzzle, year=2021, day=2):
             else: # command == "forward"
                 position += x
                 depth += aim * x
+        return position * depth
+
+    def part_2_alternative(self):
+        aim = position = depth = 0
+        for (command, x) in self.data: (aim := aim - x) if command == "up" else ((aim := aim + x) if command == "down" else (aim := aim + (x if command == "down" else -x)))
         return position * depth
 
 if(__name__ == "__main__"):
