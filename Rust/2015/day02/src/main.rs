a3df11ff52e0224882e9afe669506680c294f4da
Skulhunter5 <linus.huster@gmail.com>
2023-11-13 10:47:52 +0100

    Rust 2015 02

--- Rust/2015/day02/src/main.rs
+++ Rust/2015/day02/src/main.rs
@@ -1,17 +1,36 @@
-use std::fs;
+use std::{fs, cmp::min};
 
 fn main() {
     let input = fs::read_to_string("../inputs/02-input.txt").unwrap();
 
-    println!("Part 1: {}", part1(&input));
+    let solution = solve(&input);
+    println!("Part 1: {}", solution.0);
+    println!("Part 2: {}", solution.1);
 }
 
-fn part1(input: &String) -> u64 {
-    let mut total: u64 = 0;
+fn solve(input: &String) -> (u64, u64) {
+    let mut total_paper: u64 = 0;
+    let mut total_ribbon: u64 = 0;
     for line in input.lines() {
-        let sides = line.split("x").map(|x| x.parse::<u64>().unwrap()).collect::<Vec<_>>();
-        let faces = vec![sides[0]*sides[1], sides[0]*sides[2], sides[1]*sides[2]];
-        total += faces.iter().map(|x| x * 2).sum::<u64>() + faces.iter().min().unwrap();
+        let mut sides = line
+            .split("x")
+            .map(|x| x.parse::<u64>().unwrap())
+            .collect::<Vec<_>>();
+
+        sides.sort_unstable();
+
+        let l = sides[0];
+        let w = sides[1];
+        let h = sides[2];
+
+        let paper = 2 * l * w + 2 * l * h + 2 * w * h;
+        let slack = min(l * w, min(l * h, w * h));
+
+        let bow = l * w * h;
+        let ribbon = 2 * l + 2 * w;
+
+        total_paper += paper + slack;
+        total_ribbon += bow + ribbon;
     }
-    total
+    (total_paper, total_ribbon)
 }
