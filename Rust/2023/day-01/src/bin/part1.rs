28c2c98bd9ce0e50c941b936494c5b6db5cd5b36
Skulhunter5 <linus.huster@gmail.com>
2023-12-07 10:21:30 +0100

    Rust 2023 01 part1 rework

--- Rust/2023/day-01/src/bin/part1.rs
+++ Rust/2023/day-01/src/bin/part1.rs
@@ -4,14 +4,15 @@ fn main() {
 }
 
 pub fn solve(input: &str) -> String {
-    let mut total: u32 = 0;
-    for line in input.lines() {
-        let first = line.chars().find(|c| c.is_numeric()).unwrap();
-        let last = line.chars().rev().find(|c| c.is_numeric()).unwrap();
-        let calibration_value = format!("{first}{last}").parse::<u32>().unwrap();
-        total += calibration_value;
-    }
-    total.to_string()
+    input.lines()
+        .map(|line| {
+            let first = line.chars().find(|c| c.is_numeric()).unwrap();
+            let last = line.chars().rev().find(|c| c.is_numeric()).unwrap();
+            let calibration_value = format!("{first}{last}").parse::<u32>().unwrap();
+            calibration_value
+        })
+        .sum::<u32>()
+        .to_string()
 }
 
 #[cfg(test)]
