927fe9329e24b990f1bf43a18bb61ec6f2c42f6a
Skulhunter5 <linus.huster@gmail.com>
2025-12-10 11:23:54 +0100

    Rust solution 2025 day 03

--- Rust/2025/src/days/day03.rs
+++ Rust/2025/src/days/day03.rs
@@ -4,20 +4,42 @@ pub struct Day03;
 
 impl Day for Day03 {
     fn solve_both(&self, input: String) -> Solutions {
-        let total_output_joltage = input
-            .trim_end()
+        let input = input.trim_end();
+
+        let total_output_joltage_1 = input
             .lines()
             .map(|line| {
                 let line = line.as_bytes();
                 let first = line[..(line.len() - 1)].iter().max().unwrap();
                 let first_pos = line.iter().position(|byte| byte == first).unwrap();
                 let first = *first - b'0';
-                let second = *line[(first_pos + 1)..].iter().max().unwrap() as u8 - b'0';
+                let second = *line[(first_pos + 1)..].iter().max().unwrap() - b'0';
                 (first * 10 + second) as usize
             })
             .sum::<usize>();
 
-        return Solutions::first(total_output_joltage.to_string());
+        let total_output_joltage_2 = input
+            .lines()
+            .map(|line| {
+                let mut line = line.as_bytes();
+
+                let mut joltage: usize = 0;
+                for i in (0..12).rev() {
+                    let max = line[..(line.len() - i)].iter().max().unwrap();
+                    let pos = line.iter().position(|byte| byte == max).unwrap();
+                    line = &line[(pos + 1)..];
+                    let digit = max - b'0';
+                    joltage = joltage * 10 + digit as usize;
+                }
+
+                joltage
+            })
+            .sum::<usize>();
+
+        return Solutions::both(
+            total_output_joltage_1.to_string(),
+            total_output_joltage_2.to_string(),
+        );
     }
 
     fn get_year(&self) -> usize {
@@ -34,6 +56,9 @@ impl Day for Day03 {
 234234234234278
 818181911112111";
 
-        Some(vec![Example::new(input, Solutions::first("357"))])
+        Some(vec![Example::new(
+            input,
+            Solutions::both("357", "3121910778619"),
+        )])
     }
 }
