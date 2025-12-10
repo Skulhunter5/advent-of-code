63f7f9124130cb5afe16bd4da2b7a3e81c6446fc
Skulhunter5 <linus.huster@gmail.com>
2025-12-10 09:06:28 +0100

    Rust 2025 add option to only run specific day(s)

--- Rust/2025/src/days.rs
+++ Rust/2025/src/days.rs
@@ -8,3 +8,7 @@ mod day02;
 pub fn get_all() -> Vec<Box<dyn Day>> {
     vec![Box::new(Day01), Box::new(Day02)]
 }
+
+pub fn iter() -> std::vec::IntoIter<Box<dyn Day>> {
+    get_all().into_iter()
+}
--- Rust/2025/src/main.rs
+++ Rust/2025/src/main.rs
@@ -1,6 +1,6 @@
-use std::{env, path::Path, process::ExitCode};
+use std::{collections::HashSet, env, path::Path, process::ExitCode};
 
-use aoc_2025::days;
+use aoc_2025::{Day, days};
 
 enum Mode {
     Run,
@@ -10,25 +10,51 @@ enum Mode {
 fn main() -> ExitCode {
     let mut args = env::args();
     let _binary_path = args.next().unwrap();
-    let mode = if let Some(mode) = args.next() {
-        match mode.as_str() {
-            "test" => Mode::Test,
-            _ => panic!("invalid usage"),
+    let mut mode = None;
+    let mut days_to_run = HashSet::new();
+    while let Some(arg) = args.next() {
+        if let Some(new_mode) = match arg.as_str() {
+            "test" => Some(Mode::Test),
+            "run" => Some(Mode::Run),
+            _ => None,
+        } {
+            if mode.is_some() {
+                panic!("error: multiple modes given");
+            }
+            mode = Some(new_mode);
+        } else if ["--day"].contains(&arg.as_str()) {
+            let day = args
+                .next()
+                .expect(format!("error: missing parameter for {}", arg).as_str());
+            let day = day
+                .parse::<usize>()
+                .expect(format!("error: invalid argument: {}", day).as_str());
+            days_to_run.insert(day);
+        } else if ["--days", "-d"].contains(&arg.as_str()) {
+            let days_str = args
+                .next()
+                .expect(format!("error: missing parameter for {}", arg).as_str());
+            days_to_run.extend(days_str.split(',').map(|day| {
+                day.parse::<usize>()
+                    .expect(format!("error: invalid parameter: {}", days_str).as_str())
+            }));
+        } else {
+            panic!("error: unexpected argument: {}", arg);
         }
-    } else {
-        Mode::Run
-    };
+    }
 
-    match mode {
-        Mode::Run => run(),
-        Mode::Test => test_examples(),
+    let days_to_run =
+        days::iter().filter(|day| days_to_run.is_empty() || days_to_run.contains(&day.get_day()));
+    match mode.unwrap_or(Mode::Run) {
+        Mode::Run => run(days_to_run),
+        Mode::Test => test_examples(days_to_run),
     }
 
     return ExitCode::SUCCESS;
 }
 
-fn run() {
-    for day in days::get_all() {
+fn run<D: Iterator<Item = Box<dyn Day>>>(days_to_run: D) {
+    for day in days_to_run {
         let input = std::fs::read_to_string(Path::new("./inputs/").join(day.input_name()))
             .expect(format!("missing input: {}", day.input_name()).as_str());
         let solutions = day.solve_both(input);
@@ -42,8 +68,8 @@ fn run() {
     }
 }
 
-fn test_examples() {
-    for day in days::get_all() {
+fn test_examples<D: Iterator<Item = Box<dyn Day>>>(days_to_run: D) {
+    for day in days_to_run {
         if let Some(examples) = day.get_examples() {
             for (i, example) in examples.into_iter().enumerate() {
                 let input = example.input;
