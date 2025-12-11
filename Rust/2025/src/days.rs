a23affa3ff8b86d467aad0be9a536d75e24a9ba4
Skulhunter5 <linus.huster@gmail.com>
2025-12-11 13:58:14 +0100

    Rust solution 2025 day 06 part 1

--- Rust/2025/src/days.rs
+++ Rust/2025/src/days.rs
@@ -4,12 +4,14 @@ pub use day02::Day02;
 pub use day03::Day03;
 pub use day04::Day04;
 pub use day05::Day05;
+pub use day06::Day06;
 
 mod day01;
 mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 
 pub fn get_all() -> Vec<Box<dyn Day>> {
     vec![
@@ -18,6 +20,7 @@ pub fn get_all() -> Vec<Box<dyn Day>> {
         Box::new(Day03),
         Box::new(Day04),
         Box::new(Day05),
+        Box::new(Day06),
     ]
 }
 
---
+++ Rust/2025/src/days/day06.rs
@@ -0,0 +1,59 @@
+use crate::{Day, Example, Solutions};
+
+pub struct Day06;
+
+impl Day for Day06 {
+    fn solve_both(&self, input: String) -> Solutions {
+        let input = input.trim_end();
+        let mut lines = input
+            .lines()
+            .map(|line| line.split(' ').filter(|token| !token.is_empty()))
+            .collect::<Vec<_>>();
+        let operators = lines.pop().expect("invalid input");
+
+        let mut total = 0;
+        for operator in operators {
+            let items = lines.iter_mut().map(|line| {
+                line.next()
+                    .expect("invalid input: lines have different numbers of elements")
+            });
+            let column_result = match operator {
+                "+" => items
+                    .map(|item| {
+                        item.parse::<usize>().expect(
+                            format!("invalid input: expected a number, found '{}'", item).as_str(),
+                        )
+                    })
+                    .sum::<usize>(),
+                "*" => items
+                    .map(|item| {
+                        item.parse::<usize>().expect(
+                            format!("invalid input: expected a number, found '{}'", item).as_str(),
+                        )
+                    })
+                    .product::<usize>(),
+                x => panic!("invalid input: invalid operator: '{}'", x),
+            };
+            total += column_result;
+        }
+
+        return Solutions::first(total.to_string());
+    }
+
+    fn get_year(&self) -> usize {
+        2025
+    }
+
+    fn get_day(&self) -> usize {
+        6
+    }
+
+    fn get_examples(&self) -> Option<Vec<Example>> {
+        let input = "123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  ";
+
+        Some(vec![Example::new(input, Solutions::first("4277556"))])
+    }
+}
