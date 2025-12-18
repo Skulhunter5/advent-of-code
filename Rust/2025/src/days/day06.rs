2c1dfaccb91b3e9a82fb87a711524583edebbb11
Skulhunter5 <linus.huster@gmail.com>
2025-12-18 12:08:54 +0100

    Rust solution 2025 day 06 part 2

--- Rust/2025/src/days/day06.rs
+++ Rust/2025/src/days/day06.rs
@@ -12,25 +12,20 @@ impl Day06 {
 
         let mut total = 0;
         for operator in operators {
-            let items = lines.iter_mut().map(|line| {
-                line.next()
-                    .expect("invalid input: lines have different numbers of elements")
-            });
+            let items = lines
+                .iter_mut()
+                .map(|line| {
+                    line.next()
+                        .expect("invalid input: lines have different numbers of elements")
+                })
+                .map(|item| {
+                    item.parse::<usize>().expect(
+                        format!("invalid input: expected a number, found '{}'", item).as_str(),
+                    )
+                });
             let column_result = match operator {
-                "+" => items
-                    .map(|item| {
-                        item.parse::<usize>().expect(
-                            format!("invalid input: expected a number, found '{}'", item).as_str(),
-                        )
-                    })
-                    .sum::<usize>(),
-                "*" => items
-                    .map(|item| {
-                        item.parse::<usize>().expect(
-                            format!("invalid input: expected a number, found '{}'", item).as_str(),
-                        )
-                    })
-                    .product::<usize>(),
+                "+" => items.sum::<usize>(),
+                "*" => items.product::<usize>(),
                 x => panic!("invalid input: invalid operator: '{}'", x),
             };
             total += column_result;
@@ -41,12 +36,45 @@ impl Day06 {
 
     fn solve_second(input: &str) -> usize {
         let mut lines = input.lines().collect::<Vec<_>>();
-        let mut operators = lines.pop().expect("invalid input");
-        while let Some(operator_pos) = operators.find(|c| c != ' ') {
-            // let operator = operators.chars().nth(operator_pos);
+        let line_length = lines[0].len();
+        let operators = lines.pop().expect("invalid input");
+        let mut lines = lines.iter().map(|line| line.chars()).collect::<Vec<_>>();
+        let mut offset = 0;
+        let mut total = 0;
+        while offset < operators.len() {
+            let operator = operators.chars().nth(offset).expect("invalid input");
+            let problem_width = operators[(offset + 1)..]
+                .find(|c| c != ' ')
+                .unwrap_or(line_length - offset);
+            let items = (0..problem_width)
+                .map(|_| {
+                    let res = lines
+                        .iter_mut()
+                        .map(|line| {
+                            line.next()
+                                .expect("invalid input: lines have different lengths")
+                        })
+                        .filter(|c| *c != ' ')
+                        .collect::<String>();
+                    return res;
+                })
+                .map(|s| s.parse::<usize>().expect("invalid input"));
+            let column_result: usize = match operator {
+                '+' => items.sum(),
+                '*' => items.product(),
+                x => panic!("invalid input: invalid operator: '{}'", x),
+            };
+            total += column_result;
+
+            offset += problem_width + 1;
+            if offset < operators.len() {
+                lines.iter_mut().for_each(|line| {
+                    line.next().unwrap();
+                });
+            }
         }
 
-        todo!();
+        return total;
     }
 }
 
@@ -73,6 +101,9 @@ impl Day for Day06 {
   6 98  215 314
 *   +   *   +  ";
 
-        Some(vec![Example::new(input, Solutions::first("4277556"))])
+        Some(vec![Example::new(
+            input,
+            Solutions::both("4277556", "3263827"),
+        )])
     }
 }
