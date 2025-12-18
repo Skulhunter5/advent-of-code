77effec39c1d69545f3b021b8bf0154298f73651
Skulhunter5 <linus.huster@gmail.com>
2025-12-18 14:15:17 +0100

    Rust solution 2025 day 07 part 2

--- Rust/2025/src/days/day07.rs
+++ Rust/2025/src/days/day07.rs
@@ -6,10 +6,10 @@ impl Day for Day07 {
     fn solve_both(&self, input: String) -> Solutions {
         let mut input = input.into_bytes();
         let mut lines = input.split_mut(|byte| *byte == b'\n');
-        let start = lines
-            .next()
-            .unwrap()
-            .iter()
+        let first_line = lines.next().unwrap();
+        let line_length = first_line.len();
+        let start = first_line
+            .into_iter()
             .position(|byte| *byte == b'S')
             .expect("invalid input: no 'S' marker");
         let lines = lines
@@ -20,6 +20,10 @@ impl Day for Day07 {
 
         let beams = &mut vec![start];
         let new_beams = &mut Vec::new();
+
+        let beams2 = &mut vec![0usize; line_length];
+        beams2[start] = 1;
+        let new_beams2 = &mut vec![0usize; line_length];
 
         let split_count: usize = lines
             .map(|line| {
@@ -46,14 +50,32 @@ impl Day for Day07 {
                         }
                     }
                 }
+                new_beams2.fill(0);
+                for beam in 0..beams2.len() {
+                    let beam_count = beams2[beam];
+                    if beam_count == 0 {
+                        continue;
+                    }
+                    if line[beam] == b'^' {
+                        new_beams2[beam - 1] += beam_count;
+                        new_beams2[beam + 1] += beam_count;
+                    } else {
+                        new_beams2[beam] += beam_count;
+                    }
+                }
+
                 std::mem::swap(beams, new_beams);
                 new_beams.clear();
+
+                std::mem::swap(beams2, new_beams2);
 
                 split_count
             })
             .sum();
 
-        return Solutions::first(split_count.to_string());
+        let timelines: usize = beams2.iter().sum();
+
+        return Solutions::both(split_count.to_string(), timelines.to_string());
     }
 
     fn get_year(&self) -> usize {
