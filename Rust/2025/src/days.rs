cad28fd10b764e4820c26869a6ec836cb24221e1
Skulhunter5 <linus.huster@gmail.com>
2025-12-18 12:47:31 +0100

    Rust solution 2025 day 07 part 1

--- Rust/2025/src/days.rs
+++ Rust/2025/src/days.rs
@@ -5,6 +5,7 @@ pub use day03::Day03;
 pub use day04::Day04;
 pub use day05::Day05;
 pub use day06::Day06;
+pub use day07::Day07;
 
 mod day01;
 mod day02;
@@ -12,6 +13,7 @@ mod day03;
 mod day04;
 mod day05;
 mod day06;
+mod day07;
 
 pub fn get_all() -> Vec<Box<dyn Day>> {
     vec![
@@ -21,6 +23,7 @@ pub fn get_all() -> Vec<Box<dyn Day>> {
         Box::new(Day04),
         Box::new(Day05),
         Box::new(Day06),
+        Box::new(Day07),
     ]
 }
 
---
+++ Rust/2025/src/days/day07.rs
@@ -0,0 +1,87 @@
+use crate::{Day, Example, Solutions};
+
+pub struct Day07;
+
+impl Day for Day07 {
+    fn solve_both(&self, input: String) -> Solutions {
+        let mut input = input.into_bytes();
+        let mut lines = input.split_mut(|byte| *byte == b'\n');
+        let start = lines
+            .next()
+            .unwrap()
+            .iter()
+            .position(|byte| *byte == b'S')
+            .expect("invalid input: no 's' marker");
+        let lines = lines
+            .filter(|line| !line.is_empty())
+            .enumerate()
+            .filter(|(i, _)| i % 2 != 0)
+            .map(|(_, line)| line);
+
+        let beams = &mut vec![start];
+        let new_beams = &mut Vec::new();
+
+        let split_count: usize = lines
+            .map(|line| {
+                let mut split_count = 0;
+                for beam in &*beams {
+                    let beam = *beam;
+                    if line[beam] == b'^' {
+                        split_count += 1;
+                        if new_beams
+                            .last()
+                            .map(|last_beam| *last_beam != beam - 1)
+                            .unwrap_or(true)
+                        {
+                            new_beams.push(beam - 1);
+                        }
+                        new_beams.push(beam + 1);
+                    } else {
+                        if new_beams
+                            .last()
+                            .map(|last_beam| *last_beam != beam)
+                            .unwrap_or(true)
+                        {
+                            new_beams.push(beam);
+                        }
+                    }
+                }
+                std::mem::swap(beams, new_beams);
+                new_beams.clear();
+
+                split_count
+            })
+            .sum();
+
+        return Solutions::first(split_count.to_string());
+    }
+
+    fn get_year(&self) -> usize {
+        2025
+    }
+
+    fn get_day(&self) -> usize {
+        7
+    }
+
+    fn get_examples(&self) -> Option<Vec<Example>> {
+        let input = ".......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............";
+
+        Some(vec![Example::new(input, Solutions::first("21"))])
+    }
+}
